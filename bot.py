eb19799d19f11571893d79a2beabbb1b409861a6
Sam Brindle <brindlejsam@gmail.com>
2022-09-20 17:16:35 -0700

    worked on connecting our bot commands and the algorithm

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ load_dotenv()
 
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
-time_regex = r"([0|1]\d:[0-5]\d)|(2[0-3]:[0-5]\d)"
+time_regex = r"([0|1]\d[0-5]\d)|(2[0-3][0-5]\d)"
 
 members = {}
 events = {}
@@ -67,7 +67,7 @@ async def on_member_join(member):
 # - leave commands
 #     allows you to leave specified event
 
-# update to use server nickname?
+# ****UPDATED WITH OBJECT******
 @bot.command(name='optin', help='This command gives you the ability to join events.')
 async def opt_in(ctx):
 
@@ -76,7 +76,7 @@ async def opt_in(ctx):
     else:
         response = f'{ctx.author.name} has signed up for events!'
 
-    members[ctx.author.name] = []
+    members[ctx.author.name] = Member(ctx.author.name, [])
     print(members)
 
     await ctx.send(response)
@@ -89,34 +89,38 @@ async def opt_out(ctx):
     else:
         response = f'{ctx.author.name} has decided to opt out for events!'
 
-    members.remove(ctx.author.name)
+    members.pop(ctx.author.name)
     print(members)
     await ctx.send(response)
 
-
+# ***** FIX ISSUE OF TIME STARTING WITH 0 OR 2 ***********
 @bot.command(name='availability', help='This command allows you to enter in your typical availability')
 async def enter_availability(message):
     days_of_week = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
     user_availability = []
     await message.channel.send(f'For each days availability, please enter your available time range in a 24 hour format '
-                               f'(Ex. 15:45, 23:30) or None if not available')
+                               f'(Ex. 1545, 2330) or None if not available')
 
     for day in days_of_week:
         sleep(1)
         await message.channel.send(f'Enter your availability on {day} ->')
         msg = await bot.wait_for('message')
+        split_message = helper_check(msg.content)
 
-        if len(helper_check(msg.content)) == 2 or msg.content == "None":
-            user_availability.append(msg.content)
+        if len(split_message) == 2:
+            fixed_message = [int(split_message[0][0]), int(split_message[1][0])]
+            user_availability.append(fixed_message)
+        elif msg.content == "None":
+            user_availability.append([None])
         else:
             # let user know invalid input and give them "None" in place
             # let them know they need to redo if they don't want None
             await message.channel.send(f'Invalid Input! Your availability for {day} is now None '
                                        f'if you want to change this redo your !availability command!')
-            user_availability.append("None")
+            user_availability.append([None])
 
-    members[message.author.name] = user_availability
-    print(members)
+    members[message.author.name].availability = user_availability
+    print(members[message.author.name].availability)
 
     await message.channel.send(user_availability)
 
@@ -130,19 +134,16 @@ def helper_check(message):
 async def create_event(ctx):
     await ctx.channel.send(f'Please enter your event name ->')
     name = await bot.wait_for('message')
-    # await ctx.channel.send(f'Please enter a description for your event ->')
-    # description = await bot.wait_for('message')
-    # await ctx.channel.send(f'Please enter your event duration (2:40 would be 2 hours and 40 minutes ->')
-    # duration = await bot.wait_for('message')
+    await ctx.channel.send(f'Please enter a description for your event ->')
+    description = await bot.wait_for('message')
+    await ctx.channel.send(f'Please enter your event duration (2:40 would be 2 hours and 40 minutes ->')
+    duration = await bot.wait_for('message')
 
-    # await ctx.channel.send(f'Please enter your event start time ->')
-    # start_time = await bot.wait_for('message')
-    # await ctx.channel.send(f'Please enter your event end time ->')
-    # end_time = await bot.wait_for('message')
-
-    events[name.content] = {}
+    events[name.content] = Event(name.content, description.content, int(duration.content))
     # print([ctx.author.name, name.content, description.content, duration])
     print(events)
+
+    print(create_event_algorithm(events[name.content], members))
 
 # await create_scheduled_event(*, name, start_time, entity_type=..., privacy_level=..., channel=..., location=...,
 #                              end_time=..., description=..., image=..., reason=None)¶
@@ -217,7 +218,35 @@ async def on_member_join(member):
 # ****** BOT ALGORITHM *****
 # **************************
 
-def create_event(ctx, members):
+# Create an object that contains
+# members = list of user objects
+# - user object
+#     - guild
+#     - availability
+#     - user's name
+# - event object
+#     - guild
+#     - event name
+#     - description
+#     - duration
+
+
+class Member:
+    def __init__(self, name, availability=[], guild=1020450611826806875):
+        self.name = name
+        self.availability = availability
+        self.guild = guild
+
+
+class Event:
+    def __init__(self, name, description, duration, guild=1020450611826806875):
+        self.name = name
+        self.description = description
+        self.duration = duration
+        self.guild = guild
+
+
+def create_event_algorithm(ctx, members):
     # event = {
     #   'guild' : ctx.guild,
     #   'name' : ctx.name,
@@ -227,30 +256,33 @@ def create_event(ctx, members):
     # }
     _members = []
 
-    for member in members: ## TODO: USE SQL SEARCH TO GET GUILD MEMBERS IN BOT
-        if member.guild == ctx.guild:
-        _members.append(member)
+    for key in members.keys():
+        if members[key].guild == ctx.guild:
+            _members.append(members[key])
+    # for member in members: ## TODO: USE SQL SEARCH TO GET GUILD MEMBERS IN BOT
+    #     if member.guild == ctx.guild:
+    #         _members.append(member)
 
     if not _members:
         return 'No active members'
 
-    hours = _members[0].hours
+    hours = _members[0].availability
 
     for i in range(1, len(_members)):
         for j, hour in enumerate(hours):
-            if _members[i].hours[j][0] is None:
+            if _members[i].availability[j][0] is None:
                 hours[j] = [None]
                 continue
             if hour[0] is None:
                 continue
-            if _members[i].hours[j][1] < hour[0]:
+            if _members[i].availability[j][1] < hour[0]:
                 hours[j] = [None]
-            if _members[i].hours[j][0] > hour[1]:
+            if _members[i].availability[j][0] > hour[1]:
                 hours[j] = [None]
-            if _members[i].hours[j][0] > hour[0]:
-                hour[0] = _members[i].hours[j][0]
-            if _members[i].hours[j][1] < hour[1]:
-                hour[1] = members[i].hours[j][1]
+            if _members[i].availability[j][0] > hour[0]:
+                hour[0] = _members[i].availability[j][0]
+            if _members[i].availability[j][1] < hour[1]:
+                hour[1] = _members[i].availability[j][1]
 
     week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
     for i, hour in enumerate(hours):
@@ -259,20 +291,20 @@ def create_event(ctx, members):
         start = hour[0] % 100
         end = hour[1] % 100
         if (start + end) < 60:
-            if hour[1] - hour[0] >= ctx.length:
-                return f'{week[i]} from {hour[0]} to {hour[0] + ctx.length}'
-            if hour[1] - hour[0] < ctx.length:
+            if hour[1] - hour[0] >= ctx.duration:
+                return f'{week[i]} from {hour[0]} to {hour[0] + ctx.duration}'
+            if hour[1] - hour[0] < ctx.duration:
                 continue
         if start > end:
             actual_time = (hour[1] - end) - (hour[0] - start) - (40+(start-end))
-            if actual_time >= ctx.length:
-                return f'{week[i]} from {hour[0]} to {hour[0] + ctx.length}'
+            if actual_time >= ctx.duration:
+                return f'{week[i]} from {hour[0]} to {hour[0] + ctx.duration}'
         if start <= end:
-            actual_time = (hour[1] - end) - (hour[0] - start) + ((end-start))
-            if actual_time >= ctx.length:
-                return f'{week[i]} from {hour[0]} to {hour[0] + ctx.length}'
-
+            actual_time = (hour[1] - end) - (hour[0] - start) + (end-start)
+            if actual_time >= ctx.duration:
+                return f'{week[i]} from {hour[0]} to {hour[0] + ctx.duration}'
 
     return 'No times found'
+
 
 bot.run(TOKEN)
